5e68644c14ab9f04b3f96dd1936c5c1700ba61c5
eclipxia <cselestia@proton.me>
2025-11-12 15:20:34 -0500

    update

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,47 +1,73 @@
 local autocommands = {
-  {
-    "BufEnter",
-    {
-      pattern = {
-        "*.asm", "*.c", "*.cpp", "*.cs", "*.csx", "*.go", "*.h", "*.hpp",
-        "*.java", "*.kdl", "*.md", "*.ps1", "*.py", "*.r", "*.rs", "*.s",
-        "*.sh", "*.zig",
-      },
-      callback = function()
-        vim.opt.shiftwidth = 4
-        vim.opt.tabstop = 4
-      end,
-    },
-  },
+	{
+		"BufEnter",
+		{
+			pattern = {
+				"*.asm",
+				"*.c",
+				"*.cpp",
+				"*.cs",
+				"*.csx",
+				"*.go",
+				"*.h",
+				"*.hpp",
+				"*.java",
+				"*.kdl",
+				"*.md",
+				"*.ps1",
+				"*.py",
+				"*.r",
+				"*.rs",
+				"*.s",
+				"*.sh",
+				"*.zig",
+			},
+			callback = function()
+				vim.opt.shiftwidth = 4
+				vim.opt.tabstop = 4
+			end,
+		},
+	},
 
+	{
+		"BufWinEnter",
+		{
+			pattern = { "*.md", "*.txt", "*.tex" },
+			callback = function()
+				vim.opt.textwidth = 80
+			end,
+		},
+	},
 
-  {
-    "BufWinEnter",
-    {
-      pattern = { "*.md", "*.txt", "*.tex" },
-      callback = function()
-        vim.opt.textwidth = 80
-      end,
-    },
-  },
-
-  {
-    "BufEnter",
-    {
-      pattern = {
-        "*.css", "*.hs", "*.html", "*.ipynb", "*.js", "*.json", "*.jsx",
-        "*.lua", "*.nix", "*.rb", "*.svelte", "*.ts", "*.tsx", "*.xml",
-      },
-      callback = function()
-        vim.opt.shiftwidth = 2
-        vim.opt.tabstop = 2
-      end,
-    },
-  },
+	{
+		"BufEnter",
+		{
+			pattern = {
+				"*.css",
+				"*.hs",
+				"*.html",
+				"*.ipynb",
+				"*.js",
+				"*.json",
+				"*.jsx",
+				"*.lua",
+				"*.nix",
+				"*.rb",
+				"*.svelte",
+				"*.ts",
+				"*.tsx",
+				"*.xml",
+			},
+			callback = function()
+				vim.opt.shiftwidth = 2
+				vim.opt.tabstop = 2
+			end,
+		},
+	},
 }
 
 for _, autocmd in ipairs(autocommands) do
-  vim.api.nvim_create_autocmd(autocmd[1], autocmd[2])
+	vim.api.nvim_create_autocmd(autocmd[1], autocmd[2])
 end
 -- local grp = vim.api.nvim_create_augroup("DiagFloatOnMove", { clear = true })
 
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,3 +1,11 @@
+function ColorMyPencils(color)
+	color = color or "rose-pine"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
 return {
 	{ "Mofiqul/dracula.nvim",
   config = function()
@@ -29,7 +37,19 @@ return {
     "kartikp10/noctis.nvim",
     dependencies = { "rktjmp/lush.nvim" },
     config = function()
-      vim.cmd("colorscheme noctis")
     end,
-  },
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+				require('rose-pine').setup({
+						disable_background = true,
+				})
+
+				vim.cmd("colorscheme rose-pine")
+
+				ColorMyPencils()
+		end
+	},
 }
