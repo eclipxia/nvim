6d859d3ea0a5097d4412cafadb56c9591b835352
eclipxia <cselestia@proton.me>
2026-07-17 02:39:39 -0400

    updated config

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,5 +1,15 @@
 return {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    opts = {},
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	opts = {},
+	config = function(_, opts)
+		local wk = require("which-key")
+		wk.setup(opts)
+		wk.add({
+			{ "<leader>c", group = "code" }, -- code actions (LSP/jdtls)
+			{ "<leader>r", group = "refactor" }, -- rename, restart LSP, registers
+			{ "<leader>o", group = "organize" }, -- organize imports (jdtls)
+			{ "<leader>v", group = "test" }, -- test class/method (jdtls)
+		})
+	end,
 }
